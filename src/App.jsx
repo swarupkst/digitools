0a1076637a4a876400c16671f98ddf86875ade9e
Swarup <swarupkst@gmail.com>
2026-03-31 19:54:40 +0600

    Setup done and JSON file created

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='bg-red-200 text-center font-bold text-4xl p-4'>All Setup done and JSON data created</h1>
+    </div>
+  )
+}
+
+export default App
