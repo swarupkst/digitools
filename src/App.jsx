05febb0c8520b4a7eb8b986d8a59ea038a58923a
Swarup <swarupkst@gmail.com>
2026-03-31 22:49:45 +0600

    Navbar design complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import Navbar from './component/navbar/Navbar'
 
 const App = () => {
   return (
     <div>
-      <h1 className='bg-red-200 text-center font-bold text-4xl p-4'>All Setup done and JSON data created</h1>
+      <Navbar />
     </div>
   )
 }
